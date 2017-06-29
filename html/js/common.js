4848fed6f4b79f080c0823065245e3363a0c1f5e
anwebdevelopers <anwebdev78@gmail.com>
2017-06-29 19:57:06 +0300

    29.06.17

--- html/js/common.js
+++ html/js/common.js
@@ -11,20 +11,33 @@ $(function() {
         $('.menu').slideToggle(300);
     });
 
+    //*****************************************************
+    //Адаптивный слайдер
+    //*****************************************************
+    $('.slider').addClass('owl-carousel').owlCarousel({
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        loop: true,
+        items: 1,
+        nav: true,
+        navText: '',
+        autoplayTimeout: 3000,
+        autoplay: true,
+        smartSpeed: 1200,
+    });
+
 
     //*****************************************************
     //Chrome Smooth Scroll
     //*****************************************************
     try {
         $.browserSelector();
-        if ($("html").hasClass("chrome")) {
+        if ($('html').hasClass('chrome')) {
             $.smoothScroll();
         }
-    } catch (err) {
+    } catch (err) {};
 
-    };
-
-    $("img, a").on("dragstart", function(event) {
+    $('img, a').on('dragstart', function(event) {
         event.preventDefault();
     });
 
